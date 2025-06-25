54cb42284857817f54fd9e2d307401de99482cfe
aditya-pt1 <aditya.parshtechnologies@gmail.com>
2025-06-25 11:21:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-<<<<<<< HEAD
-import Header from './components/Header';
+import React from 'react'
+import Card from './Card'
+
 
 function App() {
-  return (
-    <Header/>
-=======
+ 
 
-function App() {
   return (
-    <div className="App">
-      <h1>Hii, This is Aditya Rastogi</h1>
-    </div>
->>>>>>> 7cc3daf688cdc9f8ad8191984ca10aaba0d20b42
-  );
+    <>
+    
+  <Card/>
+    </>
+  )
 }
 
-export default App;
+export default App
