8c450656b456b3cb415ab7e69cf239f80505a4bb
aditya-pt1 <aditya.parshtechnologies@gmail.com>
2025-06-25 10:16:24 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+<<<<<<< HEAD
+import Header from './components/Header';
+
+function App() {
+  return (
+    <Header/>
+=======
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hii, This is Aditya Rastogi</h1>
+    </div>
+>>>>>>> 7cc3daf688cdc9f8ad8191984ca10aaba0d20b42
+  );
+}
+
+export default App;
